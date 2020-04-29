55db637a567202e7af9c5658a8fa31605ccab514
Parker Soto <pdsoto@ucsc.edu>
2020-04-28 21:13:38 -0700

    Sprites now Visible, Frame Still Visible

--- src/prefabs/Foot.js
+++ src/prefabs/Foot.js
@@ -2,6 +2,7 @@ class Foot extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
+        scene.add.existing(this);
         scene.physics.add.existing(this); //add object to existing scene
         this.falling = 1;
 
--- src/prefabs/Scaraphys.js
+++ src/prefabs/Scaraphys.js
@@ -2,6 +2,7 @@ class Scaraphys extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, rollingball, playerSpeed) {
         super(scene, x, y, texture, frame);
 
+        scene.add.existing(this)
         scene.physics.add.existing(this); //add object to existing scene
 
         this.dBall = rollingball;

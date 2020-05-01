57ca44c1a70220241e87206e1476b3361edd4819
metalgaiden <musicalmiller@gmail.com>
2020-04-30 22:59:52 -0700

    added the background scroll and fixed sizes

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,13 +10,16 @@ class Play extends Phaser.Scene {
         this.load.image("dung", "./assets/dungball.png");
         this.load.image("foot", "./assets/deathfoot.png");
         this.load.image("shadow", "./assets/shadow.png")
+        this.load.image("background", "./assets/background.png")
+        this.load.image("foreground", "./assets/foreground.png")
     }
 
     create() {
         //create ground
-        this.add.rectangle(0, 443, 780, 32, 0x0000FF).setOrigin(0, 0);
+        this.background = this.add.tileSprite(0, 0, 0, 0, 'background').setOrigin(0, 0);
+        this.foreground = this.add.tileSprite(0, 0, 0, 0, 'foreground').setOrigin(0, 0);
         this.ball = new Dungball(this, game.config.width / 2 + 145, 432, 'dung').setOrigin(0.5, 0.5);
-        this.player = new Scaraphys(this, game.config.width / 2, 300, 'playerSprite', 0, this.ball, game.settings.startingSpeed).setOrigin(0, 0);
+        this.player = new Scaraphys(this, game.config.width / 2 + 60, 300, 'playerSprite', 0, this.ball, game.settings.startingSpeed).setOrigin(0, -1).setScale(.5,.5);
         this.foot = new Foot(this, 500, -1000, 'foot').setOrigin(0, 0).setScale(.5,.5);
         this.shadow = new Shadow(this, 500, 300, 'shadow').setOrigin(0, 0);
 
@@ -26,11 +29,21 @@ class Play extends Phaser.Scene {
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         this.gameOver = false;
 
-        this.physics.add.overlap(this.player, this.foot, this.setgameOver, null, this);
+        function foot_smash() {
+            if (this.foot.check_falling() == 1){
+                return true;
+            }else{
+                return false;
+            }
+        };
+
+        this.physics.add.overlap(this.player, this.foot, this.setgameOver, foot_smash, this)
     }
 
     update() {
         if(!this.gameOver) {
+            this.background.tilePositionX += 1;
+            this.foreground.tilePositionX += 2;
             this.player.update();
             if (this.foot.check_falling() == 1 || this.foot.check_falling() == 3) {
                 this.foot.update();

6c9943ee6e954b144be010abbde8e9b2c41c638a
Parker Soto <pdsoto@ucsc.edu>
2020-05-07 19:55:15 -0700

    editing instructions

--- src/scenes/Instructions.js
+++ src/scenes/Instructions.js
@@ -25,21 +25,21 @@ class Instructions extends Phaser.Scene {
             },
             fixedWidth: 0
         }
-        
+
         let centerX = game.config.width/2;
-        let centerY = game.config.height - 100;
-        let textSpacer = 16;
+        let centerY = 0 + 100;
+        let textSpacer = 32;
 
         this.add.text(centerX, centerY, 'How to Play', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY - textSpacer, 'Use the arrows to move', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY - textSpacer * 2, 'Press F to raise your ball', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY - textSpacer * 3, 'Move to increase the size of your ball', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY - textSpacer * 4, 'Enemy beetles make your ball smaller', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY - textSpacer * 5, 'Raise your ball to avoid enemy beetles', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY - textSpacer * 6, 'Bigger the ball the more points earned', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY - textSpacer * 7, 'But the bigger the ball the slower you move', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY - textSpacer * 8, 'Avoid getting squashed by the foot', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY - textSpacer * 9, 'Press (F) to countiue', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + textSpacer, 'Use the arrows to move', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + textSpacer * 2, 'Press F to raise your ball', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + textSpacer * 3, 'Move to increase the size of your ball', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + textSpacer * 4, 'Enemy beetles make your ball smaller', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + textSpacer * 5, 'Raise your ball to avoid enemy beetles', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + textSpacer * 6, 'Bigger the ball the more points earned', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + textSpacer * 7, 'But the bigger the ball the slower you move', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + textSpacer * 8, 'Avoid getting squashed by the foot', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + textSpacer * 9, 'Press (F) to countiue', menuConfig).setOrigin(0.5);
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
     }
 
